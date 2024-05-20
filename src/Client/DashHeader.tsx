3b226270e73ab733fbb136edbc56839d6ca15e0a
Dhanielgeek <dhanielknightz46@gmail.com>
2024-05-20 16:05:10 +0100

    set up mobile done

--- src/Client/DashHeader.tsx
+++ src/Client/DashHeader.tsx
@@ -1,11 +1,11 @@
 import { FaRegBell,FaCaretDown } from "react-icons/fa";
 import { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
-import { useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 // import { ChangeBar } from "../Function/Slice";
 // import Logo from '../assets/1-removebg-preview.png'
 import { PiGearSix } from "react-icons/pi";
-import { AiOutlineMenu } from "react-icons/ai";
+import { IoIosMenu } from "react-icons/io";
 import { MdOutlineClear } from "react-icons/md";
 import DashMobile from "./DashMobile";
 // import { GoTriangleRight, GoTriangleLeft } from "react-icons/go";
@@ -46,26 +46,26 @@ const HandleHeaderMenu = ()=>{
     <div className="w-[100%] h-[13%] flex justify-between items-center shadow-sm">
       <div className="mobileHeader relative">
         {
-          ToggleMenu ?  <MdOutlineClear className="text-2xl cursor-pointer" onClick={HandleHeaderMenu}/> :    <AiOutlineMenu className="text-2xl cursor-pointer" onClick={HandleHeaderMenu}/>
+          ToggleMenu ?  <MdOutlineClear className="text-2xl cursor-pointer" onClick={HandleHeaderMenu}/> :    <IoIosMenu className="text-2xl cursor-pointer" onClick={HandleHeaderMenu}/>
         }
         {
           ToggleMenu ? <DashMobile /> : null
         }
       
       </div>
-      <div className="w-[25%] h-[100%]  flex justify-center items-center flex-col">
+      <div className="w-[25%] h-[100%]   flex justify-center items-center flex-col phone:w-[30%] phone:h-[80%] smallPhone:w-[40%]">
         <p className="text-lg font-medium text-[#CCCC]">Good Morning🌞 </p>
-        <h2 className=" text-3xl font-semibold">Daniel Ben</h2>
+        <h2 className=" text-3xl font-semibold phone:text-2xl">Daniel Ben</h2>
       </div>
       <div className="w-[40%] h-[100%] flex justify-center items-center">
         <div className="w-[50px] h-[50px] rounded-full bg-[#cccc] justify-center flex items-center ">
           <FaRegBell className="text-2xl"/>
         </div>
         <div className="w-[30%] h-[100%]  justify-center gap-2 items-center flex ">
-          <div className="w-[55px] h-[55px] bg-[#cccc] rounded-full flex justify-center items-center">
+          <div className="w-[55px] h-[55px] bg-[#cccc] rounded-full flex justify-center items-center phone:hidden smallPhone:hidden">
             <p className="text-2xl">DB</p>
           </div>
-          <div className="w-[10%] h-[40%] flex relative justify-center items-center">
+          <div className="w-[10%] h-[40%] flex relative justify-center items-center phone:w-[20%]">
             <FaCaretDown className="text-2xl cursor-pointer" onClick={HandleShow}/>
             {
               ShowMenu ? (<div className="w-[15rem] h-[13rem] flex justify-around items-start px-2 flex-col bg-white shadow-md absolute top-12 right-[-37px]">
--- src/Client/DashMobile.tsx
+++ src/Client/DashMobile.tsx
@@ -2,7 +2,7 @@
 
 const DashMobile = () => {
   return (
-    <div className="w-[50rem] h-[50rem] bg-black absolute top-24">DashMobile</div>
+    <div className="w-[48.6rem] h-[36.5rem] bg-black absolute top-16 smallPhone:w-[30rem] smallPhone:top-[3rem] smallPhone:h-[24.7rem]">DashMobile</div>
   )
 }
 
