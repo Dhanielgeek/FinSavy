5d9ddb93a96c587abe2d016e6eb9eca58034423d
Dhanielgeek <dhanielknightz46@gmail.com>
2024-05-21 01:57:02 +0100

    still progress

--- src/Client/DashMobile.tsx
+++ src/Client/DashMobile.tsx
@@ -1,22 +1,46 @@
 import Logo from '../assets/1-removebg-preview.png'
+import { RxDashboard } from "react-icons/rx";
+import { GoPlus } from "react-icons/go";
+import { LuWallet } from "react-icons/lu";
+import { PiSwap,PiSignOut } from "react-icons/pi";
+import { BiMoneyWithdraw } from "react-icons/bi";
+
+
 
 const DashMobile = () => {
   return (
-    <div className="w-[48.6rem] h-[36.5rem] bg-red-500 absolute top-16 flex-col  smallPhone:w-[30rem] smallPhone:top-[3rem]  smallPhone:h-[24.7rem] flex justify-center items-center">
-    <div className="w-[100%] h-[20%] bg-green-500 flex justify-center items-center">
-        <img src={Logo} alt="" className='w-[100%] h-[100%] object-contain' />
+    <div className="w-[100%] h-[100%] ">
+    <div className="w-[100%] h-[15%] flex justify-start items-center">
+        <img src={Logo} alt="" className=' w-[40%] h-[100%] object-contain' />
+        <p className='text-xl font-medium text-blue-400'>Finsavy Trading</p>
     </div>
-    <div className='w-[100%] h-[80%] bg-pink-500'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className='w-[100%] h-[70%]  flex flex-col justify-around items-center'>
+     <div className='w-[100%] h-[14%] flex justify-start px-9 gap-2 items-center'>
+         <RxDashboard className='text-3xl' />
+         <p className='text-2xl'>Dashboard</p>
+     </div>
+     <div className='w-[100%] h-[14%] flex justify-start px-9 gap-2 items-center'>
+         <GoPlus className='text-3xl' />
+         <p className='text-2xl'>Deposit</p>
+     </div>
+     <div className='w-[100%] h-[14%] flex justify-start px-9 gap-2 items-center'>
+         <LuWallet className='text-3xl' />
+         <p className='text-2xl'>My Plans</p>
+     </div>
+     <div className='w-[100%] h-[14%] flex justify-start px-9 gap-2 items-center'>
+         <BiMoneyWithdraw className='text-3xl' />
+         <p className='text-2xl'>Withdrawal</p>
+     </div>
+     <div className='w-[100%] h-[14%] flex justify-start px-9 gap-2 items-center'>
+         <PiSwap className='text-3xl' />
+         <p className='text-2xl'>Transaction</p>
+     </div>
+    </div>
+    <div className='w-[100%] h-[15%] bg-blu flex justify-start items-center'>
+        <div className='w-[100%] h-[100%] flex justify-start px-11 gap-2 items-center'>
+            <PiSignOut className='text-3xl ' />
+            <p className='text-2xl'>Logout</p>
+            </div>
     </div>
     </div>
   )
